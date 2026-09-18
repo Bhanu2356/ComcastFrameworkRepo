e014661b3e67acbddef888f9ef9c6cdeca120aad
kumarakhilesh9334-ai <kumarakhilesh9334@gmail.com>
2026-09-18 13:31:45 +0530

    i am commitin samiksha github class

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/samikshagitpage.java
+++
@@ -1,9 +0,0 @@
-package com.comcast.crm.objectrepositoryutility;
-
-import org.testng.reporters.jq.Main;
-
-public class samikshagitpage {
-public static void main(String[] args) {
-	System.out.println(" hi i am samiksha");
-}
-}
